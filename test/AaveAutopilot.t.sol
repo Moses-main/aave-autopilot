6c096e57f0dbbaefa7023a298963f2a822c75587
Moses <moseschizaram8@gmail.com>
2025-11-03 05:36:29 +0100

    add the test for fork-blockchain

--- test/AaveAutopilot.t.sol
+++ test/AaveAutopilot.t.sol
@@ -3,17 +3,7 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 import "../src/AaveAutopilot.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract MockERC20 is ERC20 {
-    constructor() ERC20("Mock USDC", "mUSDC") {
-        _mint(msg.sender, 1_000_000 * 10**6); // 1M USDC
-    }
-    
-    function decimals() public pure override returns (uint8) {
-        return 6; // USDC has 6 decimals
-    }
-}
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract AaveAutopilotTest is Test {
     AaveAutopilot public vault;
